fc6a93b23050bb47a2a64ad9c4ccbcf0c54f81ec
Sudhir <Sudheerr08@gmail.com>
2021-06-18 14:24:48 +0530

    Code added in classes 1 and 2

--- Java_program/src/Package1/class1.java
+++ Java_program/src/Package1/class1.java
@@ -1,5 +1,10 @@
 package Package1;
 
 public class class1 {
+public static void main(String[] args)
+	
+	{
+		System.out.println("My Name is Sudhir Rathore 2");
+	}
 
 }
--- Java_program/src/Package1/class2.java
+++ Java_program/src/Package1/class2.java
@@ -1,5 +1,12 @@
 package Package1;
 
-public class class2 {
+public class class2 
+{
+	public static void main(String[] args)
+	
+	{
+		System.out.println("My Name is Sudhir Rathore 1");
+	}
+	
 
 }
